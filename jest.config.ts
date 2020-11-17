d4354ef80e418d43b22cfef2a17668c79242d388
Linbudu <48507806+linbudu599@users.noreply.github.com>
2020-11-17 06:33:33 +0000

    :white_check_mark: feat: setup client test, add lock file

---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+export default {
+  automock: false,
+  bail: 5,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["./client/src/components/*.{ts|tsx}"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/**/node_modules/"],
+  coverageProvider: "v8",
+  coverageReporters: ["json", "text", "lcov", "clover"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  notify: true,
+  preset: "ts-jest",
+  runner: "jest-runner",
+  setupFiles: ["./jest.setup.ts"],
+  setupFilesAfterEnv: ["jest-enzyme", "<rootDir>jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  testEnvironmentOptions: {
+    enzymeAdapter: "react16",
+  },
+  maxWorkers: "80%",
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    // waiting for alias configuration on Parcel & TS
+    "^@/services/(.*)$": "<rootDir>/src/services/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/src/pages/$1",
+  },
+  // TODO: config by env variables dynamically
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
+  testRunner: "jasmine2",
+  timers: "real",
+  transform: { "\\.tsx?$": "ts-jest" },
+};
