3fa362007972a8f3b4d947f7fc39ad0cbc3c17f1
linbudu599 <linbudu599@gmail.com>
2021-01-20 11:20:07 +0800

    :pencil2: docs: genql operation example\

--- server/operation/genql.ts
+++ server/operation/genql.ts
@@ -1,4 +1,10 @@
-import { createClient } from "../genql-generated";
+import {
+  createClient,
+  everything,
+  generateQueryOp,
+  QueryRequest,
+  QueryResult,
+} from "../genql-generated";
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
@@ -6,6 +12,7 @@ const client = createClient({
 });
 
 (async () => {
+  // union types
   const recipes = await client.query({
     QueryRecipeUnions: {
       __typename: true,
@@ -25,5 +32,128 @@ const client = createClient({
       },
     },
   });
-  console.log(recipes);
+
+  // should use with typescript type guards
+
+  // arguments
+  const accountRegistry = await client.mutation({
+    AccountRegistry: [
+      {
+        account: {
+          accountName: `${Math.floor(Math.random() * 100)}xxx`,
+          accountPwd: "xxxxxxxxx",
+        },
+      },
+      {
+        success: true,
+        message: true,
+        // token: true,
+      },
+    ],
+  });
+
+  console.log(accountRegistry);
+
+  // all fields
+  const saltFishes = await client.query({
+    QuerySaltFishByCoefficient: [
+      {
+        coefficient: 1,
+      },
+      {
+        ...everything,
+      },
+    ],
+  });
+  console.log(saltFishes);
+
+  // chain mode
+  const accountLogin = await client.chain.query
+    .AccountLogin({
+      account: {
+        accountName: "xxxxxxx",
+        accountPwd: "xxxxxxx",
+        loginType: "ADMIN",
+      },
+    })
+    .get({ ...everything });
+
+  console.log(accountLogin);
+
+  // use with apollo-client
+  const { query, variables } = generateQueryOp({
+    QueryAllTasks: [
+      {
+        relations: {
+          joinAssignee: true,
+        },
+      },
+      {
+        success: true,
+        message: true,
+        data: {
+          taskId: true,
+          assignee: {
+            uid: true,
+          },
+        },
+      },
+    ],
+  });
+  // GraphQL 查询语句
+  console.log(query);
+
+  // advanced type usage https://genql.now.sh/docs/usage/usage-with-other-clientss
+  // const { data, error } = useQuery(gql(query), {
+  //   variables,
+  // });
+
+  // advanced typing
+  const fields: QueryRequest = {
+    QueryAllTasks: [
+      {
+        relations: {
+          joinSubstance: true,
+        },
+      },
+      {
+        success: true,
+      },
+    ],
+  };
+
+  type ReturnedType = QueryResult<typeof fields>;
+
+  const res: ReturnedType = await client.query(fields);
+  console.log(res);
+
+  // Batch Operation
+  const batchClient = createClient({
+    url: "http://localhost:4000/graphql",
+    batch: {
+      batchInterval: 100,
+      maxBatchSize: 20,
+    },
+  });
+
+  // trigger only one network request
+  await Promise.all([
+    batchClient.query({
+      QueryAllExecutors: {
+        success: true,
+      },
+    }),
+    batchClient.query({
+      QueryAllExecutors: {
+        message: true,
+      },
+    }),
+    batchClient.query({
+      QueryAllExecutors: {
+        data: {
+          uid: true,
+        },
+      },
+    }),
+  ]);
 })();
