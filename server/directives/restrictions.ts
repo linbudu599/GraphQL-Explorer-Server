45b1f84701f27d4d4a19f9a8237d3982ed7e9a06
linbudu599 <linbudu599@gmail.com>
2021-02-04 19:25:36 +0800

    :construction: feat: Limit directives series

--- server/directives/restrictions.ts
+++ server/directives/restrictions.ts
@@ -9,7 +9,15 @@ import {
 } from "graphql";
 
 import { SchemaDirectiveVisitor } from "graphql-tools";
+import { ClassType } from "type-graphql";
+
 import { LimitedLengthScalarType } from "../scalars/LimitedLength";
+import {
+  MaxLengthScalarType,
+  MinLengthScalarType,
+  GreaterThanScalarType,
+  LessThanScalarType,
+} from "../scalars/Limit";
 
 export class LengthRestrictionDirective extends SchemaDirectiveVisitor {
   visitInputFieldDefinition(
@@ -47,3 +55,71 @@ export class LengthRestrictionDirective extends SchemaDirectiveVisitor {
     }
   }
 }
+
+export const CreateRestrictionDirectiveMixin = <TScalarType extends ClassType>(
+  directiveName: string,
+  argName: string,
+  ScalarType: TScalarType
+): typeof SchemaDirectiveVisitor => {
+  class RestrictionDirective extends SchemaDirectiveVisitor {
+    visitInputFieldDefinition(
+      field: GraphQLInputField,
+      details: {
+        objectType: GraphQLInputObjectType;
+      }
+    ) {
+      this.wrapType(field);
+    }
+
+    visitFieldDefinition(
+      field: GraphQLField<any, any>,
+      details: {
+        objectType: GraphQLObjectType | GraphQLInterfaceType;
+      }
+    ) {
+      console.log(`@${directiveName} invoked at ${field.name}`);
+      this.wrapType(field);
+    }
+
+    wrapType(field: GraphQLField<any, any> | GraphQLInputField) {
+      if (
+        field.type instanceof GraphQLNonNull &&
+        field.type.ofType instanceof GraphQLScalarType
+      ) {
+        field.type = new GraphQLNonNull(
+          new ScalarType(field.type.ofType, this.args[argName])
+        );
+      } else if (field.type instanceof GraphQLScalarType) {
+        field.type = new ScalarType(field.type, this.args[argName]);
+      } else {
+        throw new Error(`Not a scalar type: ${field.type}`);
+      }
+    }
+  }
+
+  return RestrictionDirective;
+};
+
+export const MaxLengthDirective = CreateRestrictionDirectiveMixin(
+  "maxLength",
+  "max",
+  MaxLengthScalarType
+);
+
+export const MainLengthDirective = CreateRestrictionDirectiveMixin(
+  "minLength",
+  "min",
+  MinLengthScalarType
+);
+
+export const GreaterThanDirective = CreateRestrictionDirectiveMixin(
+  "greaterThen",
+  "limit",
+  GreaterThanScalarType
+);
+
+export const LessThanDirective = CreateRestrictionDirectiveMixin(
+  "lessThan",
+  "limit",
+  LessThanScalarType
+);
